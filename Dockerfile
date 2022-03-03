c7c1a490f6c0ba411b564683494f7b0cf8c4e617
Michelle Yung <myung@email.arizona.edu>
2022-03-02 18:14:33 -0700

    detect and decode QR code from input image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3
 
 RUN apt-get update && apt-get install -y \
-	python3-opencv ca-certificates python3-dev 
+	python3-opencv ca-certificates python3-dev zbar-tools
 
 
 RUN pip install torch==1.10.2 torchvision==0.11.3 -f https://download.pytorch.org/whl/cu111/torch_stable.html
--- read_qr.py
+++ read_qr.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
+
 import requests
 import argparse
+
+from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import ZBarSymbol
+
 
 print('here')
 
@@ -21,9 +41,73 @@ parser.add_argument('--model',
                        type=str,
                        help='model file')
 
+parser.add_argument('--val',
+                       type=str,
+                       help='path to validation dataset')
+
+parser.add_argument('--labels',
+                       type=str,
+                       help='path to validation labels')
+
 
 args = parser.parse_args()
 print(args)
+
+register_coco_instances("my_dataset_val", {}, args.labels, args.val)
+qr_metadata = MetadataCatalog.get("my_dataset_val")
+print(qr_metadata)
+
+cfg = get_cfg()
+cfg.MODEL.DEVICE='cpu'
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+cfg.MODEL.WEIGHTS = args.model  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+# test image
+im = cv2.imread(args.file)
+outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+
+# v = Visualizer(im[:, :, ::-1],
+#   metadata=qr_metadata, 
+#   scale=0.5, 
+#   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+# )
+
+# out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+# cv2.imshow('out', out.get_image()[:, :, ::-1])
+
+pred_boxes = outputs["instances"].pred_boxes
+
+for bbox in pred_boxes:
+  print(bbox)
+
+  # (x0, y0, x1, y1)
+
+  x0 = round(bbox[0].item())
+  y0 = round(bbox[1].item())
+  x1 = round(bbox[2].item())
+  y1 = round(bbox[3].item())
+
+  crop_img = im[ y0:y1, x0:x1]
+#   cv2_imshow(crop_img)
+
+  # zbar
+  results = decode(crop_img, symbols=[ZBarSymbol.QRCODE])
+  print(results)
 
 # data = {
 #     'rename': args.rename,
